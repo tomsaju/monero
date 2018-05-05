229180aba539552ef7e03272480065fe7702b325
Tom Saju <tomsaju92@gmail.com>
2018-05-05 23:42:23 +0530

    add expense fragment

--- app/src/main/java/com/monero/activitydetail/fragments/AddExpenseFragment.kt
+++ app/src/main/java/com/monero/activitydetail/fragments/AddExpenseFragment.kt
@@ -3,7 +3,6 @@ package com.monero.activitydetail.fragments
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,38 +13,18 @@ import com.monero.R
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [ExpenseListFragment.OnFragmentInteractionListener] interface
+ * [AddExpenseFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [ExpenseListFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
-class ExpenseListFragment : Fragment() {
+class AddExpenseFragment : Fragment() {
 
-    // TODO: Rename and change types of parameters
-
-    private var mParam1: String? = null
-    private var mParam2: String? = null
-    private var fab:FloatingActionButton? = null
     private var mListener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            mParam1 = arguments.getString(ARG_PARAM1)
-            mParam2 = arguments.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-
-         var view:View = inflater!!.inflate(R.layout.fragment_expense_list, container, false)
-        fab = view.findViewById<FloatingActionButton>(R.id.addexpenseButton) as FloatingActionButton
-        fab?.setOnClickListener { _:View ->
-            //Show the add expense fragment
-        }
-        return view
+        return inflater!!.inflate(R.layout.fragment_add_expense, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -57,11 +36,11 @@ class ExpenseListFragment : Fragment() {
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
-        /*if (context is OnFragmentInteractionListener) {
+        if (context is OnFragmentInteractionListener) {
             mListener = context
         } else {
             throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
-        }*/
+        }
     }
 
     override fun onDetach() {
@@ -81,26 +60,5 @@ class ExpenseListFragment : Fragment() {
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExpenseListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(): ExpenseListFragment {
-            val fragment = ExpenseListFragment()
-            return fragment
-        }
     }
 }// Required empty public constructor
