3b2e1fde2d557319f233e38ff393a8c0cbf55810
Tom Saju <tom.saju@ftsindia.in>
2018-06-06 18:02:21 +0530

    added hostory fragment

---
+++ app/src/main/java/com/monero/activitydetail/fragments/HistoryFragment.kt
@@ -0,0 +1,22 @@
+package com.monero.activitydetail.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by tom.saju on 6/6/2018.
+ */
+class HistoryFragment:Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
